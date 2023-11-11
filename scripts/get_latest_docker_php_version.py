0cc251ac5212d43d383a9cc57eeb2c5471de46c6
Hirofumi Wakasugi <333180+5t111111@users.noreply.github.com>
2023-11-11 13:25:52 +0900

    add get_latest_docker_php_version script

---
+++ scripts/get_latest_docker_php_version.py
@@ -0,0 +1,37 @@
+import requests
+import re
+import time
+
+def get_latest_php_tags():
+    base_url = "https://registry.hub.docker.com/v2/repositories/library/php/tags/"
+    latest_versions = {}
+    max_pages = 10  # 最大ページ数を10に設定
+
+    def process_page(url):
+        response = requests.get(url)
+        data = response.json()
+        tags = data['results']
+
+        for tag in tags:
+            tag_name = tag['name']
+            match = re.match(r'^(\d+\.\d+)\.(\d+)$', tag_name)
+            if match:
+                major_minor = match.group(1)
+                patch = int(match.group(2))
+                if major_minor not in latest_versions or patch > latest_versions[major_minor][1]:
+                    latest_versions[major_minor] = (tag_name, patch)
+
+        return data.get('next')
+
+    next_page_url = base_url
+    page_count = 0
+    while next_page_url and page_count < max_pages:
+        next_page_url = process_page(next_page_url)
+        page_count += 1
+        time.sleep(3)  # APIリクエスト間に3秒の間隔を設ける
+
+    return {k: v[0] for k, v in latest_versions.items()}
+
+latest_tags = get_latest_php_tags()
+for version, tag in latest_tags.items():
+    print(f"Latest tag for PHP {version}: {tag}")
