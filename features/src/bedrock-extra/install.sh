63c78ae3f30ad4763168b1fba1991b378c00144b
Hirofumi Wakasugi <333180+5t111111@users.noreply.github.com>
2024-11-03 14:04:55 +0900

    xdebug installation

--- features/src/bedrock-extra/install.sh
+++ features/src/bedrock-extra/install.sh
@@ -3,8 +3,26 @@
 set -e
 
 OPTIONS_LOCALE="${LOCALE:-"none"}"
+OPTIONS_XDEBUG="${XDEBUG:-"false"}"
+OPTIONS_XDEBUG_CLIENT_HOST="${XDEBUG_CLIENT_HOST:-"localhost"}"
+OPTIONS_XDEBUG_CLIENT_PORT="${XDEBUG_CLIENT_PORT:-"9003"}"
 
+# Locale configuration
 if [ "$OPTIONS_LOCALE" != "none" ]; then
     sed -i -E "s/# (${OPTIONS_LOCALE})/\1/" /etc/locale.gen
     locale-gen
+fi
+
+# Xdebug installation
+if [ "$OPTIONS_XDEBUG" != "false" ]; then
+    pecl install xdebug
+    docker-php-ext-enable xdebug
+
+    cat <<EOL | tee /usr/local/etc/php/conf.d/xdebug.ini
+[xdebug]
+xdebug.mode=debug
+xdebug.start_with_request=yes
+xdebug.client_host=${OPTIONS_XDEBUG_CLIENT_HOST}
+xdebug.client_port=${OPTIONS_XDEBUG_CLIENT_PORT}
+EOL
 fi
