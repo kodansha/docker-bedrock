e10095bcfa1a21d011ed265e87383fa9c69c2736
Hirofumi Wakasugi <333180+5t111111@users.noreply.github.com>
2024-11-03 13:44:22 +0900

    skip locale installation in extra feature

--- features/src/bedrock-extra/install.sh
+++ features/src/bedrock-extra/install.sh
@@ -5,12 +5,6 @@ set -e
 OPTIONS_LOCALE="${LOCALE:-"none"}"
 
 if [ "$OPTIONS_LOCALE" != "none" ]; then
-    # Install packages
-    apt-get update && apt-get install -y \
-        locales \
-        && apt-get clean \
-        && rm -rf /var/lib/apt/lists/*
-
     sed -i -E "s/# (${OPTIONS_LOCALE})/\1/" /etc/locale.gen
     locale-gen
 fi
